d78d78dfe1b5a794ffd6421bd96d990083137688
Edouard Demotes <e.demotes@gmail.com>
2023-03-05 10:15:56 -0500

    feat: remove export in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,17 +15,17 @@ if [ -d /home/steam/htdocs ]; then
 fi
 
 # Set default values for environment variables
-export RCON_PASSWORD=${RCON_PASSWORD:-"$(pwgen -s 16 1)"}
-export CSS_PASSWORD=${CSS_PASSWORD:-""}
-export CSS_PORT=${CSS_PORT:-"27015"}
-export CSS_TV_PORT=${CSS_TV_PORT:-"27020"}
-export CSS_CLIENT_PORT=${CSS_CLIENT_PORT:-"27005"}
-export CSS_TICKRATE=${CSS_TICKRATE:-"100"}
-export CSS_MAXPLAYERS=${CSS_MAXPLAYERS:-"16"}
-export CSS_STARTMAP=${CSS_STARTMAP:-"de_dust2"}
-export CSS_HOSTNAME=${CSS_HOSTNAME:-"Counter-Strike: Source Server"}
-export CSS_SVLAN=${CSS_SVLAN:-"0"}
-export CSS_BOTS_FILL=${CSS_BOTS_FILL:-"0"}
+RCON_PASSWORD=${RCON_PASSWORD:-"$(pwgen -s 16 1)"}
+CSS_PASSWORD=${CSS_PASSWORD:-""}
+CSS_PORT=${CSS_PORT:-"27015"}
+CSS_TV_PORT=${CSS_TV_PORT:-"27020"}
+CSS_CLIENT_PORT=${CSS_CLIENT_PORT:-"27005"}
+CSS_TICKRATE=${CSS_TICKRATE:-"100"}
+CSS_MAXPLAYERS=${CSS_MAXPLAYERS:-"16"}
+CSS_STARTMAP=${CSS_STARTMAP:-"de_dust2"}
+CSS_HOSTNAME=${CSS_HOSTNAME:-"Counter-Strike: Source Server"}
+CSS_SVLAN=${CSS_SVLAN:-"0"}
+CSS_BOTS_FILL=${CSS_BOTS_FILL:-"0"}
 
 cd css
 ./srcds_run -game cstrike +exec server.cfg +hostname "$CSS_HOSTNAME" +sv_password "$CSS_PASSWORD" +rcon_password "$RCON_PASSWORD" +map "$CSS_STARTMAP" +sv_lan "$CSS_SVLAN" +maxplayers "$CSS_MAXPLAYERS" -port "$CSS_PORT" -tv_port "$CSS_TV_PORT" -clientport "$CSS_CLIENT_PORT" -tickrate "$CSS_TICKRATE" -bot_quota "$CSS_BOTS_FILL"
